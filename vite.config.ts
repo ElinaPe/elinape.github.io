a16fe233643ef52437ed7dd2f38be5c3447b3771
Elina P <elluuu_@hotmail.com>
2024-04-04 22:20:55 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,15 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig((configEnv) => {
-  const isDevelopment = configEnv.mode === 'development';
-
-  return {
-    plugins: [react(), ViteYaml()],
-    css: {
-      modules: {
-        generateScopedName: isDevelopment ? '[name]__[local]__[hash:base64:5]' : '[hash:base64:5]',
-      },
+export default defineConfig({
+  base: '/laskuri/', 
+  plugins: [react(), ViteYaml()],
+  css: {
+    modules: {
+      generateScopedName: '[name]__[local]__[hash:base64:5]',
     },
-    optimizeDeps: {
-      include: ['@mui/material', '@mui/icons-material'] // Lisää tähän muita kirjastoja tarpeen mukaan
-    },
-  };
+  },
+  optimizeDeps: {
+    include: ['@mui/material', '@mui/icons-material'], 
+  },
 });
