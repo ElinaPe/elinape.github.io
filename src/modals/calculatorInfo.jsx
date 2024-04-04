016f1a352fac212c72b9285945eb8e196afd8909
Elina P <elluuu_@hotmail.com>
2024-04-04 14:31:35 +0300

    diagrams working great

---
+++ src/modals/calculatorInfo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography'
+
+export default function CalculatorInfoModal({ isOpen, handleClose, title, description, formula }) {
+  return (
+    <Dialog open={isOpen} onClose={handleClose} fullWidth>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <Typography variant="body1" gutterBottom>
+            {description}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+            Laskelmien kaava on seuraava:
+        </Typography>
+        <Typography variant="body2" component="pre" style={{ fontFamily: 'monospace' }}>
+            {formula}
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
